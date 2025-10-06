24531e76720f90d1e3cb976c4d8f57506879f5eb
caerri <carrieasnow@gmail.com>
2025-10-06 01:22:01 -0600

    user input / cart

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
 import Pizza from "./Pizza";
 import Order from "./Order";
-import PizzaOfTheDay from "./PizzaOfTheDay"
+import PizzaOfTheDay from "./PizzaOfTheDay";
 import { StrictMode } from "react";
 
 const App = () => {
   return (
     <StrictMode>
       <div>
-        <h1>Padre Gino's - Order Now</h1>
+        <h1 className="logo">Padre Gino's - Order Now</h1>
         <Order />
         <PizzaOfTheDay />
       </div>
--- padre-ginos/src/usePizzaOfTheDay.jsx
+++ padre-ginos/src/usePizzaOfTheDay.jsx
@@ -1,17 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from "react";
 
 export const usePizzaOfTheDay = () => {
-    const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
-    
-    useEffect(() => {
-        async function fetchPizzaOfTheDay() {
-            const response = await fetch("/api/pizza-of-the-day");
-            const data = await response.json();
-            setPizzaOfTheDay(data);
-        }
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(
+    pizzaOfTheDay ? `pizzaOfTheDay id: ${pizzaOfTheDay.id}` : "loading",
+  );
 
-        fetchPizzaOfTheDay();
-    }, []);
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
 
-    return pizzaOfTheDay;
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
 };
